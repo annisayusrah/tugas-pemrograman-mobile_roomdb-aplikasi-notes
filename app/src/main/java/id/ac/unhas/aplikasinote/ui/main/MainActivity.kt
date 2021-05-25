516c7ad07bbda5487fe17dc959c0a75827398149
Nur Annisa Yusrah Putra Djaya <nurannisayusrahpdj@gmail.com>
2021-05-25 20:54:34 +0800

    create NoteViewModel.kt

--- app/src/main/java/id/ac/unhas/aplikasinote/ui/main/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/aplikasinote/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.ac.unhas.aplikasinote
+package id.ac.unhas.aplikasinote.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.unhas.aplikasinote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
